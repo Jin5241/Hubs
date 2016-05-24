60c7b01168e1b91cb4bf1092fb483b967e28bfa2
gzwtidus46 <gzwtidus46@outlook.com>
2016-05-24 13:03:53 +0200

    Rewrite class DBConnection

--- HUBS/src/dao/DBConnection.java
+++ HUBS/src/dao/DBConnection.java
@@ -1,19 +1,43 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import java.sql.*;
+public class DBConnection {
 
-public class DBConnection
-{
-  public static void main( String args[] )
-  {
-    Connection c = null;
-    try {
-    	c=DriverManager.getConnection("D:\SQLite\test.db");
-    } catch ( Exception e ) {
-      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-      System.exit(0);
-    }
-    System.out.println("Opened database successfully");
-  }
+	private static Connection connection = null;
+	private static Statement statement = null;
+
+	public static boolean connect() {
+		try {
+			connection = DriverManager.getConnection("jdbc:sqlite:res\\HUBS.db");
+		} catch (SQLException e) {
+			return false;
+		}
+		try {
+			statement = connection.createStatement();
+		} catch (SQLException e) {
+			return false;
+		}
+		return true;
+	}
+
+	public static ResultSet executeSelect(String sql) {
+		try {
+			return statement.executeQuery(sql);
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+
+	public static boolean executeNonSelect(String sql) {
+		try {
+			return statement.execute(sql);
+		} catch (SQLException e) {
+			return false;
+		}
+	}
 }
